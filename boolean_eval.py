2636a29d297451f02446045f5fc835795b760608
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2023-09-05 20:48:02 +0200

    finish boolean_eval.py
    i use the switch statement but it's not beautiful

--- boolean_eval.py
+++ boolean_eval.py
@@ -11,20 +11,48 @@
 
 def eval_formula(string):
     verity = []
-    evaluate = []
-
     for c in string:
         match c:
-            case '1':
+            case '1': # True
                 verity.append(True)
-            case '0':
+            case '0': # False
                 verity.append(False)
+            case '!': # Negation
+                verity[-1] = False if verity[-1] is True else True
+            case '&': # conjonction
+                r = True if verity[-1] == verity[-2] else False
+                verity.pop()
+                verity.pop()
+                verity.append(r)
+            case '|': # disjonction
+                r = True if verity[-1] or verity[-2] is True else False
+                verity.pop()
+                verity.pop()
+                verity.append(r)
+            case '^': # exclusive disjonction
+                r = False if verity[-1] == verity[-2] else True
+                verity.pop()
+                verity.pop()
+                verity.append(r)
+            case '>': # material contition
+                r = True if verity[-1] == verity[-2] or verity[-1] is True else False
+                verity.pop()
+                verity.pop()
+                verity.append(r)
+            case '=': # logical equivalence
+                r = True if verity[-1] == verity[-2] else False
+                verity.pop()
+                verity.pop()
+                verity.append(r)
             case _:
-                evaluate.append(c)
-        
-    return a
+                break
+    return verity[0]
 
+
+print(eval_formula("1!"));       # false
 print(eval_formula("10&"));      # false
+print(eval_formula("00^"));      # false
+print(eval_formula("01^"));      # true
 print(eval_formula("10|"));      # true
 print(eval_formula("11>"));      # true
 print(eval_formula("10="));      # false
