819a999950b3b25772f6f556e93fb609b51c171a
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2024-12-02 16:55:31 +0100

    Create negation_normal_form.py
    init execice 05 about the negative normal form

---
+++ ex05/negation_normal_form.py
@@ -0,0 +1,10 @@
+# Program who take a string to evaluate veracity without using arithmetic operator
+# Allowed operator: 
+#    • & (bitwise AND)
+#    • | (bitwise OR)
+#    • ^ (bitwise XOR)
+#    • << (left shift)
+#    • >> (right shift)
+#    • = (assignment)
+#    • ==, !=, <, >, <=, >= (comparison operators)
+#    • ++ or += 1 allowed only to increment index
